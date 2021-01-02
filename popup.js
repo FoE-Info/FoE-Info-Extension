0e25123bd69bbadde0f25b87f0cbeae8e85449a6
FoE-Info <61969470+FoE-Info@users.noreply.github.com>
2021-01-02 16:10:54 +0000

    Delete popup.js

--- popup.js
+++
@@ -1,8 +0,0 @@
- document.querySelector('#go-to-options').addEventListener("click", function() {
-    // console.log('options');
-    if (chrome.runtime.openOptionsPage) {
-      chrome.runtime.openOptionsPage();
-    } else {
-      window.open(chrome.runtime.getURL('options.html'));
-    }
-  }); 
