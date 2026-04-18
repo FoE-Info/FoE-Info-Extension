b9b92e97872da165094a3989ef8140182b384c52
Vivid Wombat <vividwombat@gmail.com>
2026-04-18 11:53:25 +0100

    refactor: remove @ts-nocheck from AddElement.ts

--- src/js/fn/AddElement.ts
+++ src/js/fn/AddElement.ts
@@ -1,9 +1,8 @@
-// @ts-nocheck
 import * as storage from './storage';
 
-function fCollapseIcon(id, _href, collapse) {
+function fCollapseIcon(id: string, _href: string, collapse: boolean) {
   if (document.getElementById(`${id}`) != null)
-    document.getElementById(`${id}`).outerHTML = fAddCollapseIcon(
+    document.getElementById(`${id}`)!.outerHTML = fAddCollapseIcon(
       id,
       _href,
       collapse,
@@ -11,27 +10,27 @@ function fCollapseIcon(id, _href, collapse) {
   storage.set(`${collapse}`, collapse);
 }
 
-function fAddCollapseIcon(id, _href, collapse) {
+function fAddCollapseIcon(id: string, _href: string, collapse: boolean) {
   return `<span class="header-icon material-icons-outlined md-12" id="${id}" href="#${_href}" data-bs-toggle="collapse">
     ${collapse ? 'add' : 'remove'}_circle_outline
   </span>`;
 }
 
-function fCopyButton(id, colour, pos, collapse) {
+function fCopyButton(id: string, colour: string, pos: string, collapse: boolean) {
   console.debug(collapse);
   return `<span id="${id}" class="badge rounded-pill bg-${colour} float-end ${pos}-button" style="display: ${
     collapse ? 'none' : 'block'
   }" data-i18n="copy">Copy</span>`;
 }
 
-function fCopyIcon(id, colour, pos, collapse) {
+function fCopyIcon(id: string, colour: string, pos: string, collapse: boolean) {
   console.debug(collapse);
   return `<span class="bi ${pos}-icon float-end material-symbols-outlined" id="${id}" style="display: ${
     collapse ? 'none' : 'block'
   }">content_copy</span>`;
 }
 
-function fPostButton(id, colour, pos, collapse) {
+function fPostButton(id: string, colour: string, pos: string, collapse: boolean) {
   return `<span id="${id}" class="badge rounded-pill bg-${colour} float-end ${pos}-button" style="display: ${
     collapse ? 'none' : 'block'
   }" data-i18n="post">Post</span>`;
