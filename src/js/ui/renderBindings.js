743f350ea1bb22ef8df22ec3f9807b2fced4fcbe
kronikpillow <kronikpillow@gmail.com>
2026-09-12 15:00:20 +0200

    fix(ui): restore reactive bindings dropped by sideEffects
    - Add ui/renderBindings.js as the single binding composition root
    
    - Mark renderBindings/*RenderBinding side-effectful in package.json
    
    - Guard the barrel wiring and sideEffects contract with a test

---
+++ src/js/ui/renderBindings.js
@@ -0,0 +1,9 @@
+/** Single composition root that side-effect loads the shared render bindings. */
+
+require('./armyRenderBinding.js');
+require('./bonusRenderBinding.js');
+require('./quantumRenderBinding.js');
+require('./startupRenderBinding.js');
+require('./treasuryRenderBinding.js');
+
+module.exports = {};
